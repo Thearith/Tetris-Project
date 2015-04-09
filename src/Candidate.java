e771f6e985c5e9c8c0e9d9cd1d3c9d90b6800e2e
betajulian <andre.2121@yahoo.com>
2015-04-09 14:43:36 +0800

    Update Candidate.java

--- src/Candidate.java
+++ src/Candidate.java
@@ -11,17 +11,19 @@ public class Candidate implements Comparable<Candidate>{
 	
 	void random() {
 		for (int i=0; i < weights.length; i++) {
-			weights[i] = rand.nextFloat() * (2.0f - 0.0f) - 1.0f;
+			weights[i] = rand.nextFloat() * (1.0f - 0.0f) - 1.0f;
 		}
 	}
 	
 	long fitness() {
-		long startTime = System.currentTimeMillis();
+		//long startTime = System.currentTimeMillis();
 		
 		PlayerSkeleton p = new PlayerSkeleton(this.weights);
 		p.run();
-		long endTime = System.currentTimeMillis();
-		return endTime - startTime;
+		return p.getRowsCleared();
+		
+		//long endTime = System.currentTimeMillis();
+		//return endTime - startTime;
 	}
 	
 	public int compareTo(Candidate o) {
